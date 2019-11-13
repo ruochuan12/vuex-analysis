7c36de00128f79f7fb85bde60be232aabcd729a7
luoxiaochuan <luoxiaochuan@comteck.cn>
2019-11-13 23:52:04 +0800

    update: counter/app.js 添加console.log :construction:

--- vuex/examples/counter/app.js
+++ vuex/examples/counter/app.js
@@ -3,8 +3,11 @@ import Vue from 'vue'
 import Counter from './Counter.vue'
 import store from './store'
 
-new Vue({
+console.log('line-6---store', store)
+
+var vm = new Vue({
   el: '#app',
   store,
   render: h => h(Counter)
 })
+console.log('13--line-----vm', vm)
